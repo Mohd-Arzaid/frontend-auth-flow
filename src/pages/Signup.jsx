01a6af78a099fb245b93ee5f75e412dab5b8c4d4
Mohd-Arzaid <arzaid0101030@gmail.com>
2024-12-20 23:39:29 +0530

    sign up page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,150 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Eye, EyeOff, Mail, UserRound } from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { firstName, lastName, email, password, confirmPassword } = formData;
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const handleOnChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <div className="w-1/3 bg-[#f1f5f9] rounded-xl p-6 shadow-sm">
+        <h1 className="text-center text-3xl font-bold text-[#1e3a8a]">
+          Create Account
+        </h1>
+
+        <form onSubmit={handleOnSubmit} className="flex flex-col mt-3 gap-4">
+          <div>
+            <div className="relative flex items-center">
+              <UserRound className="absolute left-0 h-5 w-5 text-gray-500" />
+              <Input
+                required
+                type="text"
+                name="firstName"
+                value={firstName}
+                onChange={handleOnChange}
+                placeholder="Your First Name"
+                className="bg-transparent border-none focus:outline-none  placeholder:text-gray-500 text-lg placeholder:text-lg pl-7"
+              />
+            </div>
+            <Separator className="border-t-2 border-[#1e3a8a] w-full" />
+          </div>
+
+          <div>
+            <div className="relative flex items-center">
+              <UserRound className="absolute left-0 h-5 w-5 text-gray-500" />
+              <Input
+                type="text"
+                name="lastName"
+                value={lastName}
+                onChange={handleOnChange}
+                placeholder="Your Last Name"
+                className="bg-transparent border-none focus:outline-none  placeholder:text-gray-500 text-lg placeholder:text-lg pl-7"
+              />
+            </div>
+            <Separator className="border-t-2 border-[#1e3a8a] w-full" />
+          </div>
+
+          <div>
+            <div className="relative flex items-center">
+              <Mail className="absolute left-0 h-5 w-5 text-gray-500" />
+              <Input
+                required
+                type="text"
+                name="email"
+                value={email}
+                onChange={handleOnChange}
+                placeholder="Your Email Address"
+                className="bg-transparent border-none focus:outline-none  placeholder:text-gray-500 text-lg placeholder:text-lg pl-7"
+              />
+            </div>
+            <Separator className="border-t-2 border-[#1e3a8a] w-full" />
+          </div>
+
+          <div>
+            <div className="relative flex items-center">
+              <Input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={password}
+                onChange={handleOnChange}
+                placeholder="Create Password"
+                className="bg-transparent border-none focus:outline-none  placeholder:text-gray-500 text-lg placeholder:text-lg pl-0"
+              />
+              <div
+                className="absolute right-0 h-6 w-6 text-gray-500 cursor-pointer"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? <Eye/> : <EyeOff />}
+              </div>
+            </div>
+            <Separator className="border-t-2 border-[#1e3a8a] w-full" />
+          </div>
+
+          <div>
+            <div className="relative flex items-center">
+              <Input
+                required
+                type={showConfirmPassword ? "text" : "password"}
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={handleOnChange}
+                placeholder="Confirm Password"
+                className="bg-transparent border-none focus:outline-none  placeholder:text-gray-500 text-lg placeholder:text-lg pl-0"
+              />
+              <div
+                className="absolute right-0 h-6 w-6 text-gray-500 cursor-pointer"
+                onClick={() => setShowConfirmPassword((prev) => !prev)}
+              >
+                {showConfirmPassword ? <Eye/> : <EyeOff />}
+              </div>
+            </div>
+            <Separator className="border-t-2 border-[#1e3a8a] w-full" />
+          </div>
+
+          <Button
+            type="submit"
+            className="w-full mt-2 rounded-lg bg-[#1e3a8a] hover:bg-[#1e3a8a]/90 border-2 border-[#1e3a8a] text-white h-12 text-lg font-semibold "
+          >
+            Submit
+          </Button>
+
+          <p className="text-center text-lg text-gray-800 ">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="text-[#1e3a8a] font-semibold transition-all duration-300"
+            >
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
