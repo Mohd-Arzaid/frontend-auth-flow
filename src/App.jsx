6048fdec39e6c86b142425b5acb5b8804c18573b
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-01-07 15:40:10 +0530

    add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,67 @@ import Login from "./pages/Login";
 import VerifyEmail from "./pages/VerifyEmail";
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
+import OpenRoute from "./authRoutes/OpenRoute";
+import PrivateRoute from "./authRoutes/PrivateRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* Route for UnAuthorized Users*/}
+      <Route
+        path="signup"
+        element={
+          <OpenRoute>
+            <Signup />
+          </OpenRoute>
+        }
+      />
 
-      <Route path="signup" element={<Signup />} />
+      <Route
+        path="login"
+        element={
+          <OpenRoute>
+            <Login />
+          </OpenRoute>
+        }
+      />
 
-      <Route path="login" element={<Login />} />
+      <Route
+        path="verify-email"
+        element={
+          <OpenRoute>
+            <VerifyEmail />
+          </OpenRoute>
+        }
+      />
 
-      <Route path="verify-email" element={<VerifyEmail />} />
+      <Route
+        path="forgot-password"
+        element={
+          <OpenRoute>
+            <ForgotPassword />
+          </OpenRoute>
+        }
+      />
 
-      <Route path="forgot-password" element={<ForgotPassword />} />
+      <Route
+        path="update-password/:token"
+        element={
+          <OpenRoute>
+            <UpdatePassword />
+          </OpenRoute>
+        }
+      />
 
-      <Route path="update-password/:token" element={<UpdatePassword />} />
-
+      {/* Route for Authorized Users*/}
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
